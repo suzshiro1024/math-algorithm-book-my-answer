78a2d307f2dd90d0f70c3033b8e8babfb4c0a96c
suzshiro1024 <tix131423113210@gmail.com>
2022-08-11 08:23:57 +0900

    update practice

---
+++ practice/is_prime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool is_prime(long long x)
+{
+  for (long long i = 2; i * i <= x; i++)
+  {
+    if (x % i == 0)
+      return false;
+  }
+  return true;
+}
+
+int main()
+{
+  long long N;
+  cin >> N;
+
+  bool prime = is_prime(N);
+
+  if (prime)
+  {
+    cout << "Prime" << endl;
+  }
+  else
+  {
+    cout << "Not Prime" << endl;
+  }
+}
