69c835d6e5f0efc29a2b240e6e783e578419c059
suzshiro1024 <tix131423113210@gmail.com>
2022-08-26 19:47:26 +0900

    update practice

---
+++ practice/point_to_line_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//(x,y)
+struct point_2D
+{
+  long long x;
+  long long y;
+};
+
+// ax + by + c = 0
+struct line
+{
+  long long a;
+  long long b;
+  long long c;
+};
+
+int main()
+{
+  struct point_2D a;
+  struct point_2D b;
+  struct point_2D c;
+
+  cin >> a.x >> a.y >> b.x >> b.y >> c.x >> c.y;
+
+  struct line f;
+  f.a = c.y - b.y;
+  f.b = -1 * (c.x - b.x);
+  f.c = c.y * (c.x - b.x) - c.x * (c.y - b.y);
+
+  double ans = abs(f.a * a.x + f.b * a.y + f.c) / sqrt(f.a * f.a + f.b * f.b);
+  cout << ans << endl;
+
+  return 0;
+}
