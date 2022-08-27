daf594c132f9b305abde4bd87c3e9ce7713eb945
suzshiro1024 <tix131423113210@gmail.com>
2022-08-27 15:51:21 +0900

    2022-08-27: ID034-ID035

---
+++ ID/ID034.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+//(x,y)
+struct point_2D
+{
+  long long x;
+  long long y;
+};
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  struct point_2D points[N + 1];
+
+  for (int i = 1; i <= N; i++)
+    cin >> points[i].x >> points[i].y;
+
+  double res = numeric_limits<double>::max();
+  for (int i = 1; i <= N; i++)
+  {
+    for (int j = 1; j <= N; j++)
+    {
+      if (i == j)
+        continue;
+
+      double dist = sqrt((points[j].x - points[i].x) * (points[j].x - points[i].x) + (points[j].y - points[i].y) * (points[j].y - points[i].y));
+      if (dist < res)
+        res = dist;
+    }
+  }
+
+  cout << res << endl;
+
+  return 0;
+}
---
+++ ID/ID035.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct point_2D
+{
+  double x;
+  double y;
+};
+
+struct circle
+{
+  point_2D center;
+  double r;
+};
+
+int main()
+{
+  circle A;
+  circle B;
+
+  point_2D center_A;
+  point_2D center_B;
+
+  cin >> center_A.x >> center_B.y >> A.r >> center_B.x >> center_B.y >> B.r;
+  A.center = center_A, B.center = center_B;
+
+  double dist = sqrt((B.center.x - A.center.x) * (B.center.x - A.center.x) + (B.center.y - A.center.y) * (B.center.y - A.center.y));
+
+  if (dist < abs(A.r - B.r))
+    cout << "type: 1" << endl;
+  else if (dist == abs(A.r - B.r))
+    cout << "type: 2" << endl;
+  else if (dist > abs(A.r - B.r) && dist < A.r + B.r)
+    cout << "type: 3" << endl;
+  else if (dist == A.r + B.r)
+    cout << "type: 4" << endl;
+  else if (dist > A.r + B.r)
+    cout << "type: 5" << endl;
+
+  return 0;
+}
